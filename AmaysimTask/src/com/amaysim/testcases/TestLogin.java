d95c7944dc03c70b63a0961d483132020e607231
Sumit Sharma <mailtosumit@gmail.com>
2017-10-15 20:14:58 +1100

    Changes

--- AmaysimTask/src/com/amaysim/testcases/TestLogin.java
+++ AmaysimTask/src/com/amaysim/testcases/TestLogin.java
@@ -5,7 +5,6 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
