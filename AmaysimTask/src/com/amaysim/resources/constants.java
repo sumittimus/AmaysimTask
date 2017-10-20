78640e21315b8a0e19a5bda593f8b4cdd2ded4ce
sumittimus <32811454+sumittimus@users.noreply.github.com>
2017-10-20 22:18:41 +1100

    Changed for Windows support

--- AmaysimTask/src/com/amaysim/resources/constants.java
+++ AmaysimTask/src/com/amaysim/resources/constants.java
@@ -4,7 +4,7 @@ public class constants {
 
 	public static final String PROPERTIES_FILE = System.getProperty("user.dir")+"/src/com/amaysim/resources/project.properties";
 	public static final String PROPERTIES_FILE_temp = System.getProperty("user.dir")+"/src/com/amaysim/resources/test.properties";
-	public static final String GECKO_PATH = System.getProperty("user.dir")+"/Lib/geckodriver";
+	public static final String GECKO_PATH = System.getProperty("user.dir")+"/Lib/geckodriver.exe";
 	
 	
 }
